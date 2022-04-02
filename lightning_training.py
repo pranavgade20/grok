d96281296af5d4c2cb2a72a6c587d7fb18b1cec0
Your Name <you@example.com>
2022-04-02 17:05:24 +0000

    Rename main training script to train.py

---
+++ lightning_training.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import comet_ml
+import gin
+import gin.torch
+
+import grok
+import os
+
+gin.parse_config(os.getenv("GIN_CONFIG", ""))
+def main():
+    parser = grok.training.training_args()
+    parser.set_defaults(logdir=os.environ.get("GROK_LOGDIR", "."))
+    hparams = parser.parse_args()
+    hparams.datadir = os.path.abspath(hparams.datadir)
+    hparams.logdir = os.path.abspath(hparams.logdir)
+
+    print(hparams)
+    print(grok.training.train(hparams))
+
+if __name__ == '__main__':
+    main()
